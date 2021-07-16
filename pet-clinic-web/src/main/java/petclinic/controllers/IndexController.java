0e0dd5d48927e6b245f849074bf4098997765784
cath0615 <md0615>
2021-07-16 20:40:40 +0800

    Adding index page and controller. closes #8.

---
+++ pet-clinic-web/src/main/java/petclinic/controllers/IndexController.java
@@ -0,0 +1,17 @@
+package petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    //when there is request come into the root or root slash context or index, index.html,
+    //they will match to this requestMapping
+    @RequestMapping({"", "/", "index.html", "index"})
+    //Thymeleaf will go back and look for a template called index
+    //this wires up the controller for the above process
+    public String index() {
+        return"index";
+    }
+}
