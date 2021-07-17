fd87b12f13e5cb20ff50649115c14848ee55e345
cath0615 <md0615>
2021-07-17 23:53:54 +0800

    Refactored Map Imlp to abstract ID generation. Closes #23

--- pet-clinic-data/src/main/java/petclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/petclinic/services/map/AbstractMapService.java
@@ -1,18 +1,17 @@
 package petclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import petclinic.model.BaseEntity;
+
+import java.util.*;
 
 //An abstract class allows you to create functionality that subclasses can implement or override.
 //An interface only allows you to define functionality, not implement it.
 //Abstract classes cannot be instantiated
 //we want to have a map implementation, a Spring Data JPA implementation so we create this class
-public abstract class AbstractMapService<T, ID> {
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
     //Protected keyword in Java refers to one of its access modifiers. The methods or data members
     //declared as protected can be accessed from: Within the same class. Subclasses of same packages.
-    protected Map<ID, T> map = new HashMap<>();
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         //return back a new hashset of values
@@ -24,8 +23,16 @@ public abstract class AbstractMapService<T, ID> {
     }
 
     //bc we don't know the specific object, we override it to include id
-    T save(ID id, T object) {
-        map.put(id, object);
+    T save(T object) {
+        if(object != null) {
+            if(object.getId() == null){
+                object.setId((getNextId()));
+            }
+
+            map.put(object.getId(), object);
+        } else {
+            throw new RuntimeException("Object cannot be null");
+        }
         return(object);
     }
 
@@ -38,4 +45,15 @@ public abstract class AbstractMapService<T, ID> {
     }
 
     //the whole class gives us a base map service that we can implement
+
+    private Long getNextId() {
+        Long nextId = null;
+        try {
+            nextId = Collections.max(map.keySet()) + 1;
+        } catch (NoSuchElementException e) {
+            nextId = 1L;
+        }
+
+        return nextId;
+    }
 }
