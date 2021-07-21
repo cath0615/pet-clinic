90491668ca9555279db183e874bf5aaf9de83a7e
cath0615 <md0615>
2021-07-22 02:13:52 +0800

    add spring JPA repositories. Closes #39

---
+++ pet-clinic-data/src/main/java/petclinic/repositories/OwnerRepository.java
@@ -0,0 +1,9 @@
+package petclinic.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import petclinic.model.Owner;
+
+//because these types are JPA entities, Spring data JPA is going to provide us instances of these
+//at runtime automatically, so we can use them in spring context
+public interface OwnerRepository extends CrudRepository<Owner, Long> {
+}
