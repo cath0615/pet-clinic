a223d10fdb3ecde6a38c1bcefc81e7bee8f98dad
cath0615 <md0615>
2021-07-22 02:28:09 +0800

    create SD JPA service for owners. Closes #40

--- pet-clinic-data/src/main/java/petclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/petclinic/repositories/OwnerRepository.java
@@ -6,4 +6,5 @@ import petclinic.model.Owner;
 //because these types are JPA entities, Spring data JPA is going to provide us instances of these
 //at runtime automatically, so we can use them in spring context
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    Owner findByLastName(String lastName);
 }
