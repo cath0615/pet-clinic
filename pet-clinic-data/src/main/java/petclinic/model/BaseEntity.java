a7694a506f6876c61612730c286e44e98d3cdec3
cath0615 <md0615>
2021-07-22 01:23:06 +0800

    implement base entity JPA. Closes #35

--- pet-clinic-data/src/main/java/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/petclinic/model/BaseEntity.java
@@ -1,9 +1,18 @@
 package petclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
+//established this class as a base class to JPA so other classes will be inheriting this class
+//we don't need this specific class mapped to the database
 public class BaseEntity implements Serializable {
 
+    @Id //tells JPA this is the id value
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
